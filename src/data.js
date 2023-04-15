df09d0cbec0d29c08f5c3f791f8151b989db7ef1
Nishant Kumar <work.nishantkumar@gmail.com>
2023-04-16 01:44:52 +0530

    data

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
 export const data=[
     {
-        title:"Title 1",
-        paragraph:"un texto lindo y largo",
+        title:"Mahatma Gandhi",
+        paragraph:"Born and raised in a Hindu family in coastal Gujarat, Gandhi trained in the law at the Inner Temple, London, and was called to the bar at age 22 in June 1891. After two uncertain years in India, where he was unable to start a successful law practice, he moved to South Africa in 1893 to represent an Indian merchant in a lawsuit. He went on to live in South Africa for 21 years. It was here that Gandhi raised a family and first employed nonviolent resistance in a campaign for civil rights. In 1915, aged 45, he returned to India and soon set about organising peasants, farmers, and urban labourers to protest against excessive land-tax and discrimination.",
         theme: {background:'lightpink',text:"black"},
     },
     {
-        title:"Title 2",
-        paragraph:"un texto lindo y largo",
+        title:"Bhagat Singh",
+        paragraph:"Bhagat Singh (27 September 1907 23 March 1931) was a charismatic Indian revolutionary who participated in the mistaken murder of a junior British police officer in what was to be retaliation for the death of an Indian nationalist. He later took part in a largely symbolic bombing of the Central Legislative Assembly in Delhi and a hunger strike in jail, which—on the back of sympathetic coverage in Indian-owned newspapers—turned him into a household name in the Punjab region, and after his execution at age 23 into a martyr and folk hero in Northern India. Borrowing ideas from Bolshevism and anarchism he electrified a growing militancy in India in the 1930s, and prompted urgent introspection within the Indian National Congress's nonviolent but eventually successful campaign for India's independence.",
         theme: {background:'lightblue',text:"black"},
     },
     {
-        title:"Title 3",
-        paragraph:"un texto lindo y largo",
+        title:"Vallabhbhai Patel",
+        paragraph:"commonly known as Sardar Patel, was an Indian lawyer, influential political leader, barrister and statesman who served as the first Deputy Prime Minister and Home Minister of India from 1947 to 1950. He was a barrister and a senior leader of the Indian National Congress, who played a leading role in the country's struggle for independence, guiding its integration into a united, independent nation.In India and elsewhere, he was often called Sardar, meaning in Hindi, Urdu, Bengali and Persian. He acted as the Home Minister during the political integration of India and the Indo-Pakistani War of 1947.",
         theme: {background:'yellow',text:"black"},
     },
 ];
